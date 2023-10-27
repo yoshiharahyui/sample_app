e5baefd3bc7060a3cd47c8445048865c289b0c7b
yoshiharahyui <147331182+yoshiharahyui@users.noreply.github.com>
2023-10-27 09:54:19 +0000

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,10 +4,7 @@ class StaticPagesController < ApplicationController
 
   def help
   end
-<<<<<<< HEAD
 
   def about
   end
-=======
->>>>>>> refs/remotes/origin/static-pages
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
   get 'static_pages/home'
   get 'static_pages/help'
-<<<<<<< HEAD
   get  "static_pages/about"
-=======
->>>>>>> refs/remotes/origin/static-pages
-  root "application#hello"
 end
