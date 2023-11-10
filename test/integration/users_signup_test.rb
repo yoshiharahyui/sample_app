a1885a35d8cbae4605ac059c527f0bb8dd6011ae
yoshiharahyui <147331182+yoshiharahyui@users.noreply.github.com>
2023-11-10 10:19:49 +0000

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -16,4 +16,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       assert_select 'div.alert'
       assert_select 'div.alert-danger'
     end
+
+    test "valid signup information" do
+      assert_difference 'User.count', 1 do
+        post users_path, params: { user: { name:  "Example User",
+                                           email: "user@example.com",
+                                           password:              "password",
+                                           password_confirmation: "password" } }
+      end
+      follow_redirect!
+      assert_template 'users/show'
+    end
 end
