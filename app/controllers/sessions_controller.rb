29b32b7ae18aa0c673b5b14a2dbee30b2f90ad75
yoshiharahyui <147331182+yoshiharahyui@users.noreply.github.com>
2023-11-12 12:01:21 +0000

    Create hamburger, and Chage a responsive design

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       reset_session      # ログインの直前に必ずこれを書くこと
       log_in user
       redirect_to user
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,10 +1,15 @@
 require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  test "login with invalid information" do
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "login with valid email/invalid password" do
     get login_path
     assert_template 'sessions/new'
-    post login_path, params: { session: { email: "", password: "" } }
+    post login_path, params: { session: { email: @user.email, password: "invalid" } }
     assert_response :unprocessable_entity
     assert_template 'sessions/new'
     assert_not flash.empty?
