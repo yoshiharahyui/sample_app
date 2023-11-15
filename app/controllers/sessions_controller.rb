70350c8561ada63a91f204985ee5cec8efcb1f62
yoshiharahyui <147331182+yoshiharahyui@users.noreply.github.com>
2023-11-15 13:07:15 +0000

    testing a check box

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
       reset_session      # ログインの直前に必ずこれを書くこと
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      remember user
-      log_in user
-      redirect_to user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      log_in @user
+      redirect_to @user
     else
       # エラーメッセージを作成する
       flash.now[:danger] = 'Invalid email/password combination'
