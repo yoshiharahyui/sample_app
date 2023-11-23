8248cc5a81dfb3624327efd9af181442332f2285
yoshiharahyui <147331182+yoshiharahyui@users.noreply.github.com>
2023-11-23 11:43:46 +0000

    Created template of mailer, Finished mailer preview

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,14 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  config.action_mailer.raise_delivery_errors = false
+
+  host = '<hex string>.app.github.dev' # ここをコピペすると失敗します。自分の環境のホストに変えてください。
+  # クラウドIDEの場合は以下をお使いください
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  # localhostで開発している場合は以下をお使いください
+  # config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
