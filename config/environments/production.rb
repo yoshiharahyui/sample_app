5f9c0da29270299266be2fe0a7141c5d8add0c01
yoshiharahyui <147331182+yoshiharahyui@users.noreply.github.com>
2023-11-25 12:38:53 +0000

    Changed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'http://127.0.0.1:3000/.onrender.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
     :port           => 587,
     :address        => 'smtp.mailgun.org',
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
