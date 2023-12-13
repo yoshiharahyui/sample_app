8681337580e426b196f51f8a5525d9c451fa3b01
yoshiharahyui <147331182+yoshiharahyui@users.noreply.github.com>
2023-12-13 11:13:27 +0000

    Createda sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,12 +17,20 @@ User.create!(name:  name,
     password:              password,
     password_confirmation: password,
     activated: true,
-               activated_at: Time.zone.now)
+    activated_at: Time.zone.now)
 end
-         
+
 # ユーザーの一部を対象にマイクロポストを生成する
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(word_count: 5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# ユーザーフォローのリレーションシップを作成する
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
